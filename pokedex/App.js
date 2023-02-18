69528cdc1fa3cd5543f0fea33989a1674e65e72d
Felicia Winna <winna.felicia@gmail.com>
2023-02-18 19:33:36 +0700

    Added bottom tab navigation to app

--- pokedex/App.js
+++ pokedex/App.js
@@ -3,24 +3,57 @@ import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import 'react-native-gesture-handler';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 import Login from './screens/Login'
 import Home from './screens/Home'
 import PokemonLibrary from './screens/PokemonLibrary';
 import RandomPokemon from './screens/RandomPokemon';
+import Profile from './screens/Profile';
 
 const Stack = createNativeStackNavigator()
+const Tab = createMaterialBottomTabNavigator();
 
 export default function AuthNavigator() {
     console.log(Stack)
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen name="Login" component={Login} /> */}
-        <Stack.Screen name="Library" component={PokemonLibrary} />
-        {/* <Stack.Screen name="Random" component={RandomPokemon} /> */}
-        <Stack.Screen name="Home" screen="Home" component={Home} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Library') {
+              iconName = focused ? 'list' : 'list-outline';
+            } 
+            else if (route.name === 'Get Random') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
+            }
+            else if(route.name === 'Profile'){
+              iconName = focused ? 'person-circle' : 'person-circle-outline'
+            }
+            else if(route.name ==="Home"){
+              iconName = focused ?  'home' : 'home-outline'
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={26} color={'#A36672'} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={Home}
+        />
+        <Tab.Screen name="Library" component={PokemonLibrary} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Get Random" component={RandomPokemon} />
+
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
